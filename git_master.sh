56b7c370c219152fe7c879d18c48f0ba7e339016
Vivek Arya <vivekveersainarya@gmail.com>
2019-10-09 00:20:12 +0530

    Pushing New Developments!

---
+++ git_master.sh
@@ -0,0 +1,9 @@
+#"""
+#@author: Vievk V. Arya [github.com/vivekveersain]
+#"""
+#go to the repository where the files are...
+#and do the following...
+git add .
+git status
+git commit -m "Pushing New Developments!"
+git push
