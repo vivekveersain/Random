9613c1dfdd2ac7b298d5f3527354de7e19bca7b5
vivekveersain <vivekveersainarya@gmail.com>
2019-10-09 00:52:10 +0530

    Minor Changes

--- git_master.sh
+++
@@ -1,9 +0,0 @@
-#"""
-#@author: Vievk V. Arya [github.com/vivekveersain]
-#"""
-#go to the repository where the files are...
-#and do the following... [next step is to get it done automatically for all repositories]
-git add .
-git status
-git commit -m "Pushing New Developments!"
-git push
