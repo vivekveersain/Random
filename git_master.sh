ea40814a5892c3be033b11daacd0aacf9e0448d0
vivekveersain <vivekveersainarya@gmail.com>
2019-10-09 00:24:17 +0530

    Pushing New Developments!

--- git_master.sh
+++ git_master.sh
@@ -2,7 +2,7 @@
 #@author: Vievk V. Arya [github.com/vivekveersain]
 #"""
 #go to the repository where the files are...
-#and do the following...
+#and do the following... [next step is to get it done automatically for all repositories]
 git add .
 git status
 git commit -m "Pushing New Developments!"
